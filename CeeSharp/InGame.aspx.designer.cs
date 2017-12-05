2f412492c5c85ba3a34d59ad1191f4474ff12a0f
teahelaschuk <teahelaschuk@gmail.com>
2017-12-04 23:53:50 -0800

    starting points and text bubbles

--- CeeSharp/InGame.aspx.designer.cs
+++ CeeSharp/InGame.aspx.designer.cs
@@ -58,6 +58,24 @@ namespace CeeSharp {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl stats;
         
         /// <summary>
+        /// Label_tcompleted control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_tcompleted;
+        
+        /// <summary>
+        /// Label_completed control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label_completed;
+        
+        /// <summary>
         /// Label_tprevious control.
         /// </summary>
         /// <remarks>
@@ -92,51 +110,6 @@ namespace CeeSharp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label_target;
-        
-        /// <summary>
-        /// Label_tcompleted control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label_tcompleted;
-        
-        /// <summary>
-        /// Label_completed control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label_completed;
-        
-        /// <summary>
-        /// Label_tmove control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label_tmove;
-        
-        /// <summary>
-        /// Label_move control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label_move;
-        
-        /// <summary>
-        /// Label_stat control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label_stat;
         
         /// <summary>
         /// Panel1 control.
